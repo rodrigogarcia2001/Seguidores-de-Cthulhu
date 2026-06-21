ec7c11ea6144a9c4163ae4b2df594898aa3ea592
BigWolf140 <ignacioalexander21@gmail.com>
2026-06-21 01:08:19 -0300

    Update del Puzzle de la puerta
    Se verifico que la puerta se borre correctamente.

--- Assets/Decorations/Usable Objects/asset_store/DoorLock.cs
+++ Assets/Decorations/Usable Objects/asset_store/DoorLock.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
+
 public class DoorLock : MonoBehaviour
 {
     public GameObject door;
+    private static bool doorDestroyed = false;
+
+    void Start()
+    {
+        if (doorDestroyed && door != null)
+        {
+            Destroy(door);
+        }
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
         {
-            if (KeyPuzzle.hasCorrectKey)
+            if (KeyPuzzle.hasCorrectKey && !doorDestroyed)
             {
                 Destroy(door);
+                doorDestroyed = true;
             }
         }
     }
